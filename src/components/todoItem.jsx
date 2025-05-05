3664fe7d4b584da98796409634a1426ed1fd4c0d
ABDERRAHMANE LAOURF <152749612+abderrahmane-laourf@users.noreply.github.com>
2025-05-05 18:50:54 +0100

    Update todoItem.jsx

--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function TodoItem({ text, onDelete }) {
+export default function TodoItem({ text }) {
   return (
     <li className="flex justify-between items-center bg-blue-100 text-blue-900 px-4 py-2 rounded-md shadow-sm">
       <span>{text}</span>
